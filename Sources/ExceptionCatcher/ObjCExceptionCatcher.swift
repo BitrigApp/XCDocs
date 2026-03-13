926091952a78792bb2b05d56f9d8622154a64389
Julian <julian@bitrig.com>
2026-03-12 22:00:35 -0700

    Resolve comments

--- Sources/ExceptionCatcher/ObjCExceptionCatcher.swift
+++ Sources/ExceptionCatcher/ObjCExceptionCatcher.swift
@@ -1,17 +1,6 @@
 import ExceptionCatcherObjC
 import Foundation
 
-package enum ExceptionCatcherError: Error { case missingResult }
-
 package func catchObjectiveCException(_ work: () -> Void) throws {
     if let error = XCDocsCatchException(work) as NSError? { throw error }
-}
-
-package func runCatchingExceptions<T>(_ work: () throws -> T) throws -> T {
-    var result: Result<T, Error>?
-    try catchObjectiveCException { do { result = .success(try work()) } catch { result = .failure(error) } }
-
-    guard let result else { throw ExceptionCatcherError.missingResult }
-
-    return try result.get()
 }
